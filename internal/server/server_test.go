3e8e39160bef4feced98907fba1496693e328d44
nodamu <nickadamu@gmail.com>
2021-10-14 22:02:10 +0000

    Added discovery test

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -2,19 +2,41 @@ package server
 
 import (
 	"context"
+	"flag"
 	api "github.com/nodamu/prof-log/api/v1"
 	"github.com/nodamu/prof-log/internal/auth"
 	"github.com/nodamu/prof-log/internal/config"
 	"github.com/nodamu/prof-log/internal/log"
 	"github.com/stretchr/testify/require"
+	"go.opencensus.io/examples/exporter"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 	"io/ioutil"
 	"net"
+	"os"
 	"testing"
+	"time"
 )
+
+var debug = flag.Bool("debug",false,"Enable observability for debugging")
+
+func TestMain(m *testing.M){
+	flag.Parse()
+	if *debug {
+		logger,err := zap.NewDevelopment()
+		if err != nil {
+			panic(err)
+		}
+
+		zap.ReplaceGlobals(logger)
+
+	}
+
+	os.Exit(m.Run())
+}
 
 func TestServer(t *testing.T) {
 	for scenario, fn := range map[string]func(
@@ -201,6 +223,41 @@ func setupTest(t *testing.T, fn func(config *Config)) (
 
 	authorize := auth.New(config.ACLModelFile, config.ACLPolicyFile)
 
+	var telemetryExporter *exporter.LogExporter
+	if *debug {
+		metricsLogFile, err := ioutil.TempFile("", "metrics-*.log")
+		require.NoError(t, err)
+		t.Logf("metrics log file: %s", metricsLogFile.Name())
+		tracesLogFile, err := ioutil.TempFile("", "traces-*.log")
+		require.NoError(t, err)
+		t.Logf("traces log file: %s", tracesLogFile.Name())
+
+		// Port details: https://www.jaegertracing.io/docs/getting-started/
+		//agentEndpointURI := "localhost:6831"
+		//collectorEndpointURI := "http://localhost:14268/api/traces"
+		//
+		//je, err := jaeger.NewExporter(jaeger.Options{
+		//	AgentEndpoint:          agentEndpointURI,
+		//	CollectorEndpoint:      collectorEndpointURI,
+		//	//Process:            "S",
+		//})
+		//
+		//if err != nil {
+		//	t.Fatalf("Failed to create the Jaeger exporter: %v", err)
+		//}
+		//
+		//trace.RegisterExporter(je)
+
+		telemetryExporter, err = exporter.NewLogExporter(exporter.Options{
+			MetricsLogFile: metricsLogFile.Name(),
+			TracesLogFile: tracesLogFile.Name(),
+			ReportingInterval: time.Second,
+		})
+		require.NoError(t, err)
+		err = telemetryExporter.Start()
+		require.NoError(t, err)
+	}
+
 	cfg = &Config{
 		CommitLog:  clog,
 		Authorizer: authorize,
@@ -219,7 +276,11 @@ func setupTest(t *testing.T, fn func(config *Config)) (
 		rootConn.Close()
 		nobodyConn.Close()
 		l.Close()
-		clog.Remove()
+		if telemetryExporter != nil {
+			time.Sleep(1500 * time.Millisecond)
+			telemetryExporter.Stop()
+			telemetryExporter.Close()
+		}
 	}
 }
 
